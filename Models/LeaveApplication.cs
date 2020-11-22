105ad1f103d4f93ff072a7778a34df7df8e0c519
Mqondisi Mathuthu <mqondisimathuthu@gmail.com>
2020-11-22 20:25:55 +0200

    Another commit df

---
+++ Models/LeaveApplication.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project2.Models
+{
+    public class LeaveApplication
+    {
+        public int Id { get; set; }
+        public int numberofdays { get; set; }
+    }
+}
